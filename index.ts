0d871ecba207ccdae4d27994d52f8e2bf23f3050
VladimirZhdanov <VladimirZhdanov1992@gmail.com>
2018-08-26 13:30:03 +0600

    Export RequestContext

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@ export * from "./src/KoaApiResource";
 export * from "./src/ApiListResponse";
 export * from "./src/BaseApiResource";
 export * from "./src/ApiResourceOptions";
+export * from "./src/RequestContext";
